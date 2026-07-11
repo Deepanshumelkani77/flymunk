38efd5801d899a17d12f0b73b4e99252fee0ffa4
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-07-11 14:40:33 +0530

    all done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold m-auto">welcome to flymunk</h1>
+    </div>
+  )
+}
+
+export default App
